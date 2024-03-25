1eebc53e967fc3d9249e7cfc8ef55e5d4d65c13b
cwlearner <infrastructure@dragonholdings.co>
2024-03-25 00:08:39 -0400

    new project since no Support response

---
+++ Number guessing game.py
@@ -0,0 +1,2 @@
+#Number guessing game. The goal is to make someone guess a number between 1 and 100. It will tell them if they were correct or not and record number of attempts.
+# Long term goal would be hosting in AWS and requiring user registration and keeping track of their high scores in a database.
